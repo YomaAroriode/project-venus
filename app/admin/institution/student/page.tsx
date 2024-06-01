6de683dfa045e1f70d8a0add465a6244a8b72bc6
YomaAroriode <yomaaroriode@gmail.com>
2024-06-01 18:23:00 +0100

    added student and teacher page

---
+++ app/admin/institution/student/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import Wrapper from "@/components/wrapper";
+
+export default function Student() {
+  return (
+    <>
+      <Header title="Students" />
+      <Wrapper>
+        <div>Students page</div>
+      </Wrapper>
+    </>
+  );
+}
---
+++ app/admin/institution/teacher/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import Wrapper from "@/components/wrapper";
+
+export default function Teacher() {
+  return (
+    <>
+      <Header title="Form Teachers" />
+      <Wrapper>
+        <div>Teachers page</div>
+      </Wrapper>
+    </>
+  );
+}
